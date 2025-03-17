50e577b079dc51dc4a42f3a1d6ec056ccc59f8b4
Jamous Bitrick <jbitrick@bnt.com>
2025-03-17 14:49:10 -0400

    Website update

--- gitUpdate.sh
+++ gitUpdate.sh
@@ -1,5 +1,15 @@
+# Clean out cached build files
+rm -rf build/_build/html/*
+rm -rf docs/*
+
+# Rebuild site
+make -C build/ html
+
+#Copy over .nojekyll and docs
 cp .nojekyll  docs/
 cp CNAME docs/
+
+#Push updates
 git add .
 git commit -m "Website update"
 git push
